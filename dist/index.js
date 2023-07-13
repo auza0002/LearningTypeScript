f2cb2a069f9fb53d2db9a963500e1cb0510b2749
Diego Auza <auza0002@algonquinlive.com>
2023-07-13 08:30:13 -0500

    tipos de datos

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let mensaje = "Hola Mundo";
+mensaje = "feliz cumpleano";
+console.log(mensaje);
+mensaje = "chao mundo";
+console.log(mensaje);
+console.log(typeof []);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+let mensaje: string = "Hola Mundo";
+
+mensaje = "feliz cumpleano";
+
+console.log(mensaje);
+mensaje = "chao mundo";
+console.log(mensaje);
+console.log(typeof []);
+
+/*
+number
+string
+boolea
+null
+undefined
+object
+function
+*/
